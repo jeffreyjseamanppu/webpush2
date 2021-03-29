6ba2cdd962c7448eab6fa35404ce6c6c0592f58f
LandShark <jseaman@pointpark.edu>
2021-03-29 15:50:52 -0400

    added changes

--- push.js
+++ push.js
@@ -9,6 +9,15 @@ let vapidKeys = {
 }
 
 push.setVapidDetails('mailto:jseaman@pointpark.edu', vapidKeys.publickey, vapidKeys.privatekey);
-let sub = {};
+let sub = {
+    endpoint:
+      'https://fcm.googleapis.com/fcm/send/d__zTvVuEXw:APA91bHEcOAMUgTwlr5bHHz_wxOIXiam4xpEIttAMJyW8JXbXqnn6nJsnT0eKerg6uzqeOltIp3UHnKkphoWiHK2yX3qNY97B1UNt5B9_m550BP-u2fBxk3JQptUvUBTuE_qTZ2d-DCy',
+    expirationTime: null,
+    keys: {
+      p256dh:
+        'BA9g3XSsyZGZBeTwQJ1kKUdTXzyn46NgjOhT_5KL_xblkCArJCwD05l4YwuJC1n8c2jjIMpYa5e3vGna2J_Kwsw',
+      auth: '5NsEh20fxb4Exkg6EZMwYA'
+    }
+  };
 
 push.sendNotification(sub, 'Testing this');
